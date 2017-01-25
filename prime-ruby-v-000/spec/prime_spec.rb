d64c244d9ce96c74d6d21417850b37e4f921f481
thebrianemory <the@brianemory.com>
2017-01-24 23:33:32 -0800

    Done

---
+++ prime-ruby-v-000/spec/prime_spec.rb
@@ -0,0 +1,24 @@
+require_relative './spec_helper'
+require_relative '../prime.rb'
+
+describe "prime?" do
+  it 'returns true for prime numbers' do
+    expect(prime?(3)).to be(true)
+    expect(prime?(5)).to be(true)
+    expect(prime?(7)).to be(true)
+    expect(prime?(11)).to be(true)
+    expect(prime?(105557)).to be(true)
+  end
+
+  it 'returns false for non-prime numbers' do
+    expect(prime?(-1)).to be(false), "Be sure to account for negative numbers!"
+    expect(prime?(0)).to be(false)
+    expect(prime?(1)).to be(false)
+    expect(prime?(4)).to be(false)
+    expect(prime?(9)).to be(false)
+    expect(prime?(15)).to be(false)
+    expect(prime?(40)).to be(false)
+    expect(prime?(1763)).to be(false)
+    expect(prime?(101013)).to be(false)
+  end
+end
