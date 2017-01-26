bb64cf820b2f66d9e8b4686958af13c5bb841514
thebrianemory <the@brianemory.com>
2017-01-26 09:33:35 -0800

    Done

---
+++ nested-hash-iteration-code-along-v-000/lib/contacts.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+  # This is the array we will be passing into the remove_strawberry method
+  # contacts = {
+  #   "Jon Snow" => {
+  #     name: "Jon",
+  #     email: "jon_snow@thewall.we",
+  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  #   },
+  #   "Freddy Mercury" => {
+  #     name: "Freddy",
+  #     email: "freddy@mercury.com",
+  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  #   }
+  # }
+
+def remove_strawberry(contacts)
+  contacts.each do |name, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_ice_cream_flavors
+        data[:favorite_ice_cream_flavors].delete_if { |i| i == "strawberry" }
+      end
+    end
+  end
+end
