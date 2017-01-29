d428ad9b37bbe41ad2d933fe2766d0bab5669062
thebrianemory <the@brianemory.com>
2017-01-29 13:42:38 -0800

    Done

---
+++ school-domain-v-000/lib/school.rb
@@ -0,0 +1,25 @@
+require "pry"
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.collect do |key, values|
+      @roster[key].sort!
+    end
+    @roster
+  end
+end
