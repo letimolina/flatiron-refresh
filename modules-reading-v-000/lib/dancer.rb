202fe00ed90ee31c6f14452ccfdf16edb1532ce9
thebrianemory <the@brianemory.com>
2017-02-01 20:21:27 -0800

    Done

---
+++ modules-reading-v-000/lib/dancer.rb
@@ -0,0 +1,13 @@
+require_relative './fancy_dance.rb'
+
+class Dancer
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
