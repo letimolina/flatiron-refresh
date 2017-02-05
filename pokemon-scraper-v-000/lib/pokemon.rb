64d44326496b3d9cd4942f1b15418f730a090515
thebrianemory <the@brianemory.com>
2017-02-05 14:07:49 -0800

    Done

---
+++ pokemon-scraper-v-000/lib/pokemon.rb
@@ -0,0 +1,25 @@
+require "pry"
+class Pokemon
+  attr_accessor :name, :type, :db, :id, :hp
+
+  def initialize(name:, type:, db:, id:, hp: 60)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+    @hp = hp
+  end
+
+  def self.save(name, type, database)
+    database.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id).flatten
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
+  end
+
+  def alter_hp(hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, self.id)
+  end
+end
