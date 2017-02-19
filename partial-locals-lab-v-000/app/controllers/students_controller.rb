a80bbf9e546a13325c501036b46af9782cb321e6
thebrianemory <the@brianemory.com>
2017-02-18 21:10:09 -0800

    Done

---
+++ partial-locals-lab-v-000/app/controllers/students_controller.rb
@@ -0,0 +1,34 @@
+class StudentsController < ApplicationController
+  def index
+    if params[:search]
+      @students = Student.search(params[:search])
+    else
+      @students = Student.all
+    end
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to @student
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :birthday, :hometown)
+  end
+end
---
+++ partial-locals-lab-v-000/app/models/student.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  hometown   :string
+#  birthday   :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Student < ActiveRecord::Base
+  has_many :classroom_students
+  has_many :classrooms, through: :classroom_students
+
+  def self.search(search)
+    where('name LIKE ?', "%#{search}%")
+  end
+end
