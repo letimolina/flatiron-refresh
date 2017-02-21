b7d40358da8425046b0507e9d3ef1c7f1226040b
thebrianemory <the@brianemory.com>
2017-02-21 10:40:17 -0800

    Done

---
+++ namespaced-routes-lab-v-000/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  namespace :admin do
+    resources :preferences, only: [:index, :update]
+  end
+  resources :artists do
+    resources :songs, only: [:index, :show]
+  end
+  resources :songs
+
+end
